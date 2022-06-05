e817014295bd685a3b3222020a819c83b1ece140
Diego Garber <diego.garber@unxdigital.com>
2022-06-05 17:06:58 -0300

    wrapped commit getter in useCallback

--- front/src/components/CommitList/CommitList.jsx
+++ front/src/components/CommitList/CommitList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router";
 import { fetchCommits } from "../../utils/http";
 
@@ -9,25 +9,36 @@ const CommitList = () => {
 
   const getCommits = async() => {
     try {
-      const data = await fetchCommits();
-      const commitMessages = getCommitsMessages(data.data)
+      const {data} = await fetchCommits();
+      const commitMessages = getCommitsMessages(data)
       setCommits(commitMessages);
     } catch (e) {
       alert(e.message);
     }
   }
-
-  const getCommitsMessages = (commitArray) => {
-    const messages = commitArray.map(commit => commit.commit.message)
+  const getCommitsB = useCallback(
+    async() => {
+      try {
+        const data = await fetchCommits();
+        const commitMessages = getCommitsMessages(data.data)
+        setCommits(commitMessages);
+      } catch (e) {
+        alert(e.message);
+      }
+    },
+    [],
+  );
+  const getCommitsMessages = (commitArray = []) => {
+    const messages = commitArray.map(commit => commit?.commit?.message)
     return messages
   }
 
   useEffect(() => {
     async function s() {
-      //await getCommits()
+      await getCommitsB()
     }
     s()
-  }, []);
+  }); 
 
   return (
     <div className="container">
